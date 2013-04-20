0f8749e3289c0ba637a842dc7c17082fb12e7d86
Soeren Pedersen <speconsult@gmail.com>
2013-04-20 14:45:09 +0200

    initial

---
+++ src/main/java/webdevsamples/bean/PersonDao.java
@@ -0,0 +1,34 @@
+package webdevsamples.bean;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import webdevsamples.qualifier.DataRepository;
+import webdevsamples.model.Person;
+
+/**
+ * Simple PersonDao bean that handles persisting a person entity and returning a
+ * list of people in the database. Implemented as an EJB for the transaction
+ * facilities. 
+ * 
+ */
+@Stateless
+public class PersonDao {
+
+	@Inject
+	@DataRepository
+	private EntityManager entityManager;
+
+	public void savePerson(Person p) {
+		entityManager.persist(p);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Person> getPeople() {
+		return entityManager.createQuery("select p from Person p")
+				.getResultList();
+	}
+}
