fdebd7e72d7ea9f4c5bfb12019c3aa3626340b78
Soeren Pedersen <speconsult@gmail.com>
2013-04-21 10:15:39 +0200

    added more resources

--- src/main/java/traveltours/rest/service/ResourceService.java
+++ src/main/java/traveltours/rest/service/ResourceService.java
@@ -9,28 +9,14 @@ import java.util.List;
 
 public abstract class ResourceService<T extends Resource> {
 
-    @Path("/create/")
-    @PUT
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public abstract T create(MultivaluedMap<String, String> formParams);
 
-    @Path("/read/{id}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public abstract T read(String id);
 
-    @Path("/update")
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public abstract T update(MultivaluedMap<String, String> formParams);
 
-    @Path("/delete")
-    @DELETE
     public abstract void delete(T resource);
 
-    @Path("/list")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public abstract List<T> all();
 
 }
--- src/main/java/traveltours/rest/service/TravelService.java
+++ src/main/java/traveltours/rest/service/TravelService.java
@@ -11,8 +11,12 @@ import java.util.List;
 @Path("/travel")
 public class TravelService extends ResourceService<Travel> {
 
-    private Travel currentStub;
+    private static Travel currentStub;
 
+    @Path("/create")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public Travel create(MultivaluedMap<String, String> formParams) {
         System.out.println("TravelService.create: " + formParams);
 
@@ -29,19 +33,32 @@ public class TravelService extends ResourceService<Travel> {
         return travel;
     }
 
+    @Path("/read/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Travel read(String id) {
         System.out.println("TravelService.read      **************************************");
         return currentStub;
     }
 
+    @Path("/update")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
     public Travel update(MultivaluedMap<String, String> formParams) {
         return currentStub;
     }
 
+
+    @Path("/delete")
+    @DELETE
     public void delete(Travel travel) {
         this.currentStub = null;
     }
 
+    @Path("/list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Travel> all() {
         List<Travel> travels = new ArrayList();
         travels.add(currentStub);
