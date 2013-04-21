f65f7faee58fe0c2b01c8e2a3f42c5c2feca2037
Soeren Pedersen <speconsult@gmail.com>
2013-04-21 22:29:13 +0200

    Added angular-twitter-bootstrap

--- src/main/webapp/resources/script/traveltours.js
+++ src/main/webapp/resources/script/traveltours.js
@@ -7,11 +7,24 @@ travelTours.controller('AlertDemoCtrl', function AlertDemoCtrl($scope) {
     ];
 
     $scope.addAlert = function() {
-        $scope.alerts.push({msg: "Another alert!"});
+        $scope.alerts.push({
+            msg: 'Another alert!',
+            type: 'error'
+
+        });
     };
 
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
     };
 
+});
+
+travelTours.controller("CarouselDemoCtrl", function CarouselDemoCtrl($scope) {
+    $scope.myInterval = 5000;
+    $scope.slides = [
+        {image: 'resources/images/greece_first.jpg',text: 'Greece - Rhodos'},
+        {image: 'resources/images/greece_second.jpg',text: 'Greece - Molivos'},
+        {image: 'resources/images/greece_third.jpg',text: 'Greece - Lesbos'}
+    ];
 });
