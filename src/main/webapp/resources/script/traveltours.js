056ef05bd15989e2d0960ce826696a497b08ee66
Soeren Pedersen <speconsult@gmail.com>
2013-04-21 17:12:52 +0200

    Added angular-twitter-bootstrap

--- src/main/webapp/resources/script/traveltours.js
+++ src/main/webapp/resources/script/traveltours.js
@@ -1,1 +1,17 @@
-var travelTours = angular.module('TravelTours', []);
+var travelTours = angular.module('TravelTours', ['ui.bootstrap']);
+
+travelTours.controller('AlertDemoCtrl', function AlertDemoCtrl($scope) {
+    $scope.alerts = [
+        { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' },
+        { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: "Another alert!"});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+});
