f1d85de0718ed2561e61c9f31adffc577d70e4ed
Soeren Pedersen <speconsult@gmail.com>
2013-04-28 19:12:55 +0200

    Introduced countryService

--- src/main/webapp/app/traveltours/admin/country/country.js
+++ src/main/webapp/app/traveltours/admin/country/country.js
@@ -27,18 +27,25 @@ traveltours.admin.country.CreateController = function ($scope, $routeParams, $re
 
 traveltours.admin.country.UpdateController = function ($scope, $routeParams, $resource, countryService) {
 
-    $scope.addedArea;
+    $scope.newArea = undefined;
 
     var country = countryService.read($routeParams.id, function (country) {
         $scope.country = country;
     });
 
-    $scope.addArea = function (area) {
-        $scope.country.areas.push(area);
-        $scope.addedArea = undefined;
+    $scope.addArea = function() {
+        //push an empty area into country.areas
+        $scope.country.areas.push($scope.newArea);
+        $scope.newArea = undefined;
     };
 
-    $scope.update = function (country) {
+    $scope.removeArea = function(area) {
+        var areas = $scope.country.areas;
+        removeElement(areas, area);
+    };
+
+    $scope.updateCountry = function (country) {
+        var country = $scope.country;
         console.log("updating country with id: " + country.id);
         countryService.update(country, function (country) {
             $scope.go('/admin/country/show/' + country.id);
